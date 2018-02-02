dc3a86410bbb58b2e8ef201fce0414ee9e2841bd
abtExp <atworkstudios@gmail.com>
2018-02-02 15:51:04 +0530

    fixing updates

---
+++ test/testFile.js
@@ -0,0 +1,16 @@
+import './someDir/index';
+import './resources/i1';
+import './resources/i2';
+import './resources/i3';
+import './resources/i4';
+import './resources/i5';
+import './resources/i6';
+import './resources/i7';
+import './resources/i15';
+import './resources/i11';
+import './resources/i10';
+import './resources/i11';
+import './resources/i12';
+import './resources/i13';
+import './resources/i14';
+import './resources/i15';
