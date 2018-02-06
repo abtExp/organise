8a2cdc09c122e4bdd881e013437202f46420e389
abtExp <atworkstudios@gmail.com>
2018-02-06 12:44:00 +0530

    fixing calcRelPath

---
+++ test/testFile.js
@@ -0,0 +1,10 @@
+import './someDir/index';
+import './resources/i1';
+import './resources/i2';
+import './resources/i3';
+import './resources/i4';
+import './resources/i5';
+import './resources/i6';
+import './resources/i7';
+import './resources/i8';
+import './resources/i9';
